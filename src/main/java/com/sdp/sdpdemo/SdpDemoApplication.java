881dc8a972507c6baea411471ae85b72cf7e7b1d
LuvoZulu <zululuvo03@gmail.com>
2025-07-28 17:29:11 +0200

    intellij initial project setup

---
+++ src/main/java/com/sdp/sdpdemo/SdpDemoApplication.java
@@ -0,0 +1,16 @@
+package com.sdp.sdpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+SdpDemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SdpDemoApplication.class, args);
+
+    }
+
+}
