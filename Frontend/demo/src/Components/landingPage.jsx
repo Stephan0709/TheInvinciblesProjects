eb5ac97a3de51b67a9fe62203288bf7011b0fca9
pythonreus <kgadiselepe11@mail.com>
2025-08-19 10:38:53 +0200

    added more components

--- Frontend/demo/src/Components/landingPage.jsx
+++ Frontend/demo/src/Components/landingPage.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 function Greeting(){
-	return <h1> Hello, Welcome to SmartBet!<h1>;
+	return <div>
+		    <h1> Hello, Welcome to SmartBet!</h1>;
+		    <p>This is a stat betting app</p>
+		</div>
 }
 
 export default Greeting;
