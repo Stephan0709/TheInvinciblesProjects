ddc62f861763a05d78f40287871cebd6aa053ab0
LuvoZulu <zululuvo03@gmail.com>
2025-08-21 12:18:19 +0200

    port potential fixes

--- Backend/src/main/java/com/smartbet/WebConfig.java
+++ Backend/src/main/java/com/smartbet/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 //** means apply to all url
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3002")
+                        .allowedOrigins("http://localhost:8080")
                         .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
